eca449853b2557e8e4ceef0014327cd9c43a16c8
shykulamykhailo <lucamike332@gmail.com>
2024-09-23 23:51:48 +0300

    fetching cabin list. loading spinner

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,12 +1,15 @@
-import Counter from '@/app/_components/Counter';
 import CabinCard from '@/app/_components/CabinCard';
+import { getCabins } from '../_lib/data-service';
 
 export const metadata = {
     title: 'Cabins',
 };
 
-export default function Page() {
-    const cabins = [];
+export default async function Page() {
+    console.log('starting');
+    const cabins = await getCabins();
+
+    console.log(cabins);
 
     return (
         <div>
