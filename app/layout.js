ddb17737484944af9d64cfb7f42bedae92a60b1c
shykulamykhailo <lucamike332@gmail.com>
2024-09-23 14:25:02 +0300

    header and homepage

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,36 @@
-import Logo from './components/Logo';
-import Navigation from './components/Navigation';
+import Logo from '@/app/_components/Logo';
+import Navigation from '@/app/_components/Navigation';
+
+import { Josefin_Sans } from 'next/font/google';
+
+const josefin = Josefin_Sans({
+    subsets: ['latin'],
+    display: 'swap',
+});
+
+import '@/app/_styles/globals.css';
+import Header from './_components/Header';
 
 export const metadata = {
-    title: 'The Wild Oasis',
+    title: {
+        template: '%s / The Wild Oasis',
+        default: 'Welcome / The Wild Oasis',
+    },
+    description:
+        'Luxurious cabin hotel, located in the heart of the italian Dolomites, surrounded by beautiful mountains and dark forests',
 };
 
 export default function RootLayout({ children }) {
     return (
         <html lang="eng">
-            <body>
-                <header>
-                    <Logo />
-                    <Navigation />
-                </header>
-                <main>{children}</main>
-                <footer>Copyright by the Wild Oasis</footer>
+            <body
+                className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
+            >
+                <Header />
+
+                <div className="flex-1 px-8 py-12">
+                    <main className="max-w-7xl mx-auto">{children}</main>
+                </div>
             </body>
         </html>
     );
