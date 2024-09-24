3240d4dc737cb69294f854c37bb3dc28a34b0f05
shykulamykhailo <lucamike332@gmail.com>
2024-09-24 23:27:57 +0300

    error. caching settings

---
+++ app/_components/CabinList.js
@@ -0,0 +1,21 @@
+import { unstable_noStore as noStore } from 'next/cache';
+import CabinCard from './CabinCard';
+import { getCabins } from '../_lib/data-service';
+
+async function CabinList() {
+    // noStore(); //make only this component dynamic
+
+    const cabins = await getCabins();
+
+    if (!cabins.length) return null;
+
+    return (
+        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
+            {cabins.map((cabin) => (
+                <CabinCard cabin={cabin} key={cabin.id} />
+            ))}
+        </div>
+    );
+}
+
+export default CabinList;
--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,16 +1,14 @@
-import CabinCard from '@/app/_components/CabinCard';
-import { getCabins } from '../_lib/data-service';
+import CabinList from '../_components/CabinList';
+import { Suspense } from 'react';
+import Spinner from '../_components/Spinner';
+
+export const revalidate = 3600;
 
 export const metadata = {
     title: 'Cabins',
 };
 
-export default async function Page() {
-    console.log('starting');
-    const cabins = await getCabins();
-
-    console.log(cabins);
-
+export default function Page() {
     return (
         <div>
             <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -25,14 +23,9 @@ export default async function Page() {
                 The perfect spot for a peaceful, calm vacation. Welcome to
                 paradise.
             </p>
-
-            {cabins.length > 0 && (
-                <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-                    {cabins.map((cabin) => (
-                        <CabinCard cabin={cabin} key={cabin.id} />
-                    ))}
-                </div>
-            )}
+            <Suspense fallback={<Spinner />}>
+                <CabinList />
+            </Suspense>
         </div>
     );
 }
