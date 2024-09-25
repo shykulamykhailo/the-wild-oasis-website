23a53d51a82f1eb2e2dd23f58622bd81d662bc8c
shykulamykhailo <lucamike332@gmail.com>
2024-09-25 21:17:43 +0300

    reservations

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -2,6 +2,7 @@ import CabinList from '../_components/CabinList';
 import { Suspense } from 'react';
 import Spinner from '../_components/Spinner';
 import Filter from '../_components/Filter';
+import ReservationReminder from '../_components/ReservationReminder';
 
 export const revalidate = 3600; // works only in static pages
 
@@ -35,6 +36,7 @@ export default function Page({ searchParams }) {
 
             <Suspense fallback={<Spinner />} key={filter}>
                 <CabinList filter={filter} />
+                <ReservationReminder />
             </Suspense>
         </div>
     );
