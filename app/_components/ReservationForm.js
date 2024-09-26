ff6bf8e25251772f2cd4c39f43779f9b101f650b
shykulamykhailo <lucamike332@gmail.com>
2024-09-26 22:17:43 +0300

    Authentication

--- app/_components/ReservationForm.js
+++ app/_components/ReservationForm.js
@@ -2,7 +2,7 @@
 
 import { useResevation } from './ReservationContext';
 
-function ReservationForm({ cabin }) {
+function ReservationForm({ cabin, user }) {
     const { range } = useResevation();
     // CHANGE
     const { maxCapacity } = cabin;
@@ -12,20 +12,18 @@ function ReservationForm({ cabin }) {
             <div className="bg-primary-800 text-primary-300 px-16 py-2 flex justify-between items-center">
                 <p>Logged in as</p>
 
-                {/* <div className='flex gap-4 items-center'>
-          <img
-            // Important to display google profile images
-            referrerPolicy='no-referrer'
-            className='h-8 rounded-full'
-            src={user.image}
-            alt={user.name}
-          />
-          <p>{user.name}</p>
-        </div> */}
+                <div className="flex gap-4 items-center">
+                    <img
+                        // Important to display google profile images
+                        referrerPolicy="no-referrer"
+                        className="h-8 rounded-full"
+                        src={user.image}
+                        alt={user.name}
+                    />
+                    <p>{user.name}</p>
+                </div>
             </div>
-            <p>
-                {String(range.from)} to {String(range.to)}
-            </p>
+
             <form className="bg-primary-900 py-10 px-16 text-lg flex gap-5 flex-col">
                 <div className="space-y-2">
                     <label htmlFor="numGuests">How many guests?</label>
